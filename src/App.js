c0a381e28995e4b32ea25b85dc9b775315c8d3c0
Hoang Quyen <hoangquyen18012001@gmail.com>
2022-06-10 09:18:44 +0700

    Confirm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import DetailSong from "./components/DetailSong";
+import ListSongs from "./components/ListSongs";
+import { Songs } from "./Context";
+import DataSongs from "./data/songs.json";
+import Playing from "./components/Playing";
+import { useState } from "react";
 function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1>Welcome to my Spotify App Fake!!</h1>
-			</header>
-		</div>
-	);
+  const [song, setSong] = useState(DataSongs[0])
+
+  const handleSetSong = (idSong) => {
+    const song = DataSongs.find(song => song.id === idSong)
+    if(!song) 
+           setSong(DataSongs[0])
+    else
+           setSong(song)
+  }
+  return (
+    <div className="App">
+      <Songs.Provider value={{ DataSongs, song, handleSetSong}}>
+        <Navbar />
+        <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
+          {/* span 1 */}
+          <DetailSong />
+          {/* span 2 */}
+          <ListSongs />
+        </div>
+        <Playing/>
+      </Songs.Provider>
+    </div>
+  );
 }
 
 export default App;
